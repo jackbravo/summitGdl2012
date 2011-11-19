74f818054ca7aac6f7be5f2f18c15525f890614c
David Valdez <me@gnuget.org>
2011-11-18 23:04:10 -0600

    Dandole diseño al footer

--- page/page-footer.tpl.php
+++ page/page-footer.tpl.php
@@ -1,19 +1,25 @@
-     <div class="footer">
-        <?php print $footer; ?>
-        <div id="links">
-          <div id="site-map">
-            <?php 
-              print i18nmenu_translated_tree('menu-site-map');
-            ?>
-          </div>
+    <div class="footer_content">
+       <div class="footer container_12">
+          <?php print $footer; ?>
           <div id="links">
-            <?php  print i18nmenu_translated_tree('menu-links');?>
+            <div id="site-map" class="grid_4">
+              <h3><?php print t('Site Map');?></h3>
+              <?php print i18nmenu_translated_tree('menu-site-map'); ?>
+            </div>
+            <div id="links" class="grid_4">
+              <h3><?php print t('Links');?></h3>
+              <?php  print i18nmenu_translated_tree('menu-links');?>
+            </div>
+            <div id="contact" class="grid_4">
+              <h3><?php print t('Contact');?></h3>
+            </div>
           </div>
-          <div id="contact">
+          <div class="clear"></div>
+          <div id="copyright">
+            <span>DrupalSummitLatino copyright 2011</span>
           </div>
-        </div>
-        <span>DrupalSummitLatino copyright 2011</span>
-    </div>
+      </div>
+    </div> 
     <?php print $closure;?>
     </body>
 </html>
