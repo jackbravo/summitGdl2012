c8d88602c29c0330ec5ec4371e9088c6440abc78
David Valdez <me@gnuget.org>
2011-11-21 03:08:46 -0600

    agregando sidebar paginas interiores
    También ya le dí un poco de formato a las paginas interiores

--- page/page.tpl.php
+++ page/page.tpl.php
@@ -1,9 +1,17 @@
 <?php print $page_header;?>
 <div class="container_12">
-  <div class="content">
-    <?php print $tabs;?>
-    <?php print $messages;?>
-    <?php print $content;?>
-  </div> 
+  <div class="bg <?php print (!$is_front) ? " interiores-bg" : "";?>">
+    <div class="content<?php print (!$is_front) ? " interiores grid_8" : "";?>">
+      <?php print $tabs;?>
+      <?php print $messages;?>
+      <?php print $content;?>
+    </div> 
+    <?php if(!$is_front && !empty($sidebar)):?>
+      <div class="sidebar grid_4">
+        <?php print $sidebar;?>
+      </div>
+    <?php endif;?>
+    <div class="clear"></div>
+  </div>
 </div>
 <?php print $page_footer;
